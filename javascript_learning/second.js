c552161d153e31a5e8d9534eb9ea42cceed9ee1f
Prince Yadav <76997609+pr194@users.noreply.github.com>
2024-01-09 07:23:45 +0000

    second

---
+++ javascript_learning/second.js
@@ -0,0 +1,6 @@
+let name="prince"
+var surname="yadav";
+const address="jaipur"
+//use less var because it create problem in scope accesing or functional scope
+
+console.table([name,surname,address])
