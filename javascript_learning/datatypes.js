ecd862d969766c4d312d963ebb7dc36e5b74b48d
Prince Yadav <76997609+pr194@users.noreply.github.com>
2024-01-09 07:41:12 +0000

    third

---
+++ javascript_learning/datatypes.js
@@ -0,0 +1,12 @@
+let name="prince"
+let age=23
+let isAdult=true;
+prince={
+  Name:"yadav",
+  age:63
+
+}
+console.table([prince,name,age,isAdult])
+//there are also undefined and null that are treated types of data types
+// type of null is object 
+// type of undefined is undefined
